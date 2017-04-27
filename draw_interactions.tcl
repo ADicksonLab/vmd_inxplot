ba0add2aad2149362de1edcc88a9d8315b0b0f0c
salotz <salotz@salotz.info>
2017-04-27 17:46:12 -0400

    added draw contacts functions script

---
+++ draw_interactions.tcl
@@ -0,0 +1,72 @@
+proc draw_interactions { mol pairs_file color radius} {
+
+    # read in contact pairs
+    set file [open $pairs_file]
+
+    # read in each line from the file
+    while {[gets $file cline] >= 0} {
+        # split on whitespace
+        set inds [split $cline ","]
+        # set variables from each row
+        set ind1 [lindex $inds 0]
+        set ind2 [lindex $inds 1]
+
+        # make two selections for each atom
+        set sel1 [atomselect $mol "serial $ind1"]
+        set sel2 [atomselect $mol "serial $ind2"]
+        # assign the coordinates to two variables
+        lassign [$sel1 get {x y z}] start
+        lassign [$sel2 get {x y z}] end
+
+        # set the color for the graphics
+        graphics $mol color $color
+        # draw the cylinder
+        graphics $mol cylinder $start $end radius $radius
+    }
+    close $file
+}
+
+
+proc draw_interaction { mol serial_a serial_b color radius} {
+
+    # make two selections for each atom
+    set sel1 [atomselect $mol "serial $serial_a"]
+    set sel2 [atomselect $mol "serial $serial_b"]
+    # assign the coordinates to two variables
+    lassign [$sel1 get {x y z}] start
+    lassign [$sel2 get {x y z}] end
+
+    # set the color for the graphics
+    graphics $mol color $color
+    # draw the cylinder
+    graphics $mol cylinder $start $end radius $radius
+}
+
+
+proc draw_interaction_lines { mol pairs_file color width} {
+
+    # read in contact pairs
+    set file [open $pairs_file]
+
+    # read in each line from the file
+    while {[gets $file cline] >= 0} {
+        # split on whitespace
+        set inds [split $cline ","]
+        # set variables from each row
+        set ind1 [lindex $inds 0]
+        set ind2 [lindex $inds 1]
+
+        # make two selections for each atom
+        set sel1 [atomselect $mol "serial $ind1"]
+        set sel2 [atomselect $mol "serial $ind2"]
+        # assign the coordinates to two variables
+        lassign [$sel1 get {x y z}] start
+        lassign [$sel2 get {x y z}] end
+
+        # set the color for the graphics
+        graphics $mol color $color
+        # draw the cylinder
+        graphics $mol line $start $end width $width
+    }
+    close $file
+}
